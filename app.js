18314605872e4ef7ac94a31fd042a8eb4dd13e56
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2021-09-01 08:13:05 +0100

    all solved including bonus

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Register the location for handlebars partials here:
 
@@ -19,7 +20,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render("index")
 });
+
+/*
+app.get('/beers', (req, res) => {
+  //Promises
+  //Promises are JavaScript Objects that might
+  //have or not a value in the near future
+
+  /* approach one
+  punkAPI.getBeers().then(response =>{
+    console.log(response); //it doesnt have to response it oculd be beers
+    res.render('beers', {response});
+  }) */
+  //.catch(error =>{
+  //  console.log(error);
+  //})  //catches errors and returns them
+/*
+});
+*/
+
+//modern way of consuming beers
+app.get('/beers', async (req, res) => {  
+  const beers = await punkAPI.getBeers();
+  res.render("beers", {beers});
+});
+
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom().then(response =>{
+    console.log(response);
+    res.render('random-beer', {response});
+  })
+.catch(error =>{
+  console.log(error);
+})  
+
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
